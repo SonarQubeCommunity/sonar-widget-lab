cb61906b8005b0b06ad23280d3f6fb97fc2d51e0
Ann Campbell <ann.campbell@shawinc.com>
2012-08-07 14:45:31 +0000

    updated to v3.2 of Sonar added ability to use Markdown switched 'title' render to use the widget title (blue bar) fcnality userText widget property now a TEXT type (so you get a textarea to type in)

--- src/main/java/org/codehaus/sonar/plugins/widgetlab/HtmlWidget.java
+++ src/main/java/org/codehaus/sonar/plugins/widgetlab/HtmlWidget.java
@@ -21,17 +21,21 @@ package org.codehaus.sonar.plugins.widgetlab;
 
 import org.sonar.api.web.*;
 
-
 @UserRole(UserRole.USER)
 @Description("Displays the configured text. Intended to enable dashboard description.")
 @WidgetCategory({"Global"})
 @WidgetScope("GLOBAL")
 @WidgetProperties({
     @WidgetProperty(key = "userText",
-        description = "Text to display in widget"
+        description = "Text to display in widget",
+        type=WidgetPropertyType.TEXT
     ),
-    @WidgetProperty(key = "title",
-        description = "Widget title"
+    @WidgetProperty(key = "widgetTitle",
+        description = "Widget title. Will be displayed in a blue bar across the top."
+    ),
+    @WidgetProperty(key = "isMarkdown",
+        description = "Check to have text rendered as markdown",
+        type=WidgetPropertyType.BOOLEAN
     )
 })
 public class HtmlWidget  extends AbstractRubyTemplate implements RubyRailsWidget {
